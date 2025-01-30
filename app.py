7f674c67af05ed0500bd2540923feceff8fe87b0
RedShadow <marcuzg@gmail.com>
2025-01-30 13:10:55 +0100

    Static commit

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__, template_folder='templates')
 def index():
     return render_template('index.html')
 
+@app.route('/register')
+def dashboard():
+    return render_template('register.html')
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
