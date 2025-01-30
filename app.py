b6322c5e2711f4587704c629f134039bc629330c
RedShadow <marcuzg@gmail.com>
2025-01-30 12:44:41 +0100

    Changing Dir Structures

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
-app.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-app.route('/dashboard')
+@app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5555, debug=True)
