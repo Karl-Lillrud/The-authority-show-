c69bacaf44e67a81135d46c853177ecc77361e2e
RedShadow <marcuzg@gmail.com>
2025-01-31 13:55:42 +0100

    blaha

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 @app.route('/register')
 def register():
