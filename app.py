c70ddd5e73f10fab0133337154de825f2a857d33
RedShadow <marcuzg@gmail.com>
2025-01-31 14:39:00 +0100

    tjoho

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html")s
 
 @app.route('/register')
 def register():
