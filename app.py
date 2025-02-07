61a5f477855a7c627d83ece0e1a5e6cbe307397b
RedShadow <marcuzg@gmail.com>
2025-02-07 14:49:24 +0100

    Added GET method to endpoints

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify, url_for, session, redirect
-from flask_cors import CORS
 from azure.cosmos import CosmosClient
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -46,13 +45,7 @@ EMAIL_PASS = os.getenv("EMAIL_PASS")
 # Determine API base URL based on environment
 API_BASE_URL = "http://127.0.0.1:8000" if APP_ENV == "local" else "https://app.podmanager.ai"
 
-# Enable CORS for local and production environments
-if APP_ENV == "local":
-    CORS(app, origins="http://127.0.0.1:8000")  # Allow localhost for local dev
-else:
-    CORS(app, origins="https://app.podmanager.ai")  # Only allow the production domain
-
-@app.route('/forgotpassword', methods=['POST'])
+@app.route('/forgotpassword', methods=['GET','POST'])
 def forgot_password():
     if request.content_type != 'application/json':
         return jsonify({"error": "Invalid Content-Type. Expected application/json"}), 415  # Unsupported Media Type
@@ -83,7 +76,7 @@ def forgot_password():
         return jsonify({"error": str(e)}), 500
 
 
-@app.route('/enter-code', methods=['POST'])
+@app.route('/enter-code', methods=['GET','POST'])
 def enter_code():
     if request.content_type != 'application/json':
         return jsonify({"error": "Invalid Content-Type. Expected application/json"}), 415
@@ -110,7 +103,7 @@ def enter_code():
     return jsonify({"message": "Code verified. You can now reset your password.", "redirect_url": url_for('reset_password')}), 200
 
 
-@app.route('/reset-password', methods=['POST'])
+@app.route('/reset-password', methods=['GET','POST'])
 def reset_password():
     if request.content_type != 'application/json':
         return jsonify({"error": "Invalid Content-Type. Expected application/json"}), 415
