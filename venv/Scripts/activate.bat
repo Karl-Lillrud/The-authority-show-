84ee42095c07692d597130c97a389652a4a01023
Rishad Mahmudi <rismah1102@gmail.com>
2025-02-11 09:59:31 +0100

    ,

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Sarwar\The-authority-show-\venv"
+set "VIRTUAL_ENV=C:\Users\Risha\The-authority-show-\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
