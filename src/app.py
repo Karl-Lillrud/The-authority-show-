f075c087380cfbd8e2835695e46ca61e6a244fe0
Arinsz <arin.sz@hotmail.com>
2025-02-18 23:55:54 +0100

    test

--- src/app.py
+++ src/app.py
@@ -10,11 +10,16 @@ from flask import (
     Blueprint,
 )
 from flask_cors import CORS
+from routes.register import register_bp
+from routes.forgot_pass import forgotpass_bp
+from routes.signin import signin_bp
+from routes.register_podcast import registerpodcast_bp
+from routes.dashboard import dashboard_bp
+from routes.pod_management import dashboardmanagement_bp
 from dotenv import load_dotenv
 import os
 import logging
 from utils import venvupdate
-from blueprint_register import register_blueprints
 
 # Checking if the environment variable is set to skip the virtual environment update
 if os.getenv("SKIP_VENV_UPDATE", "false").lower() not in ("true", "1", "yes"):
@@ -37,15 +42,19 @@ CORS(
 
 app.secret_key = os.getenv("SECRET_KEY")
 app.config["PREFERRED URL SCHEME"] = "https"
-
-# Register blueprints dynamically
-register_blueprints(app, "routes", [os.path.dirname(__file__) + "/routes"])
+app.register_blueprint(register_bp)
+app.register_blueprint(forgotpass_bp)
+app.register_blueprint(signin_bp)
+app.register_blueprint(registerpodcast_bp)
+app.register_blueprint(dashboard_bp)
+app.register_blueprint(dashboardmanagement_bp)
 
 APP_ENV = os.getenv("APP_ENV", "production")  # Default to production
 
 API_BASE_URL = (
     "http://127.0.0.1:8000" if APP_ENV == "local" else "https://app.podmanager.ai/"
 )
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -65,7 +74,6 @@ def health_check():
 
 
 if __name__ == "__main__":
-    logger.info("Starting the Flask application")
     app.run(
         host="0.0.0.0", port=8000, debug=False
-    )  # Ensure the port matches your request URL and enable debug mode for local environment
+    )  # Ensure the port matches your request URL
