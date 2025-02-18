fae23c9c43c86655e8c99edf5aa1f280c85c7863
RedShadow <marcuzg@gmail.com>
2025-02-18 14:48:43 +0100

    updated env and API BASE URLs

--- src/app.py
+++ src/app.py
@@ -5,27 +5,35 @@ import os
 from database.mongo_connection import collection
 from utils import venvupdate
 from blueprint_register import register_blueprints  # Correct import statement
-
-#venvupdate.update_venv_and_requirements()
-
+ 
+venvupdate.update_venv_and_requirements()
+ 
 load_dotenv()
+ 
 app = Flask(__name__, template_folder="templates", static_folder="static")
+ 
 app.secret_key = os.getenv("SECRET_KEY")
-app.config["PREFERRED URL SCHEME"] = "https"
+ 
+ 
+app.config["PREFERRED_URL_SCHEME"] = "https"
+ 
+if os.getenv('FLASK_ENV') == 'production':
+    app.config['API_BASE_URL'] = os.getenv("PROD_BASE_URL")
+else:
+    app.config['API_BASE_URL'] = os.getenv("LOCAL_BASE_URL")
+ 
 register_blueprints(app, "routes", [os.path.dirname(__file__) + "/routes"])
+ 
 CORS(app)
-
-
-# Pass API_BASE_URL to templates
+ 
 @app.context_processor
 def inject_api_base_url():
-    return dict(API_BASE_URL=os.getenv("API_BASE_URL"))
-
-
+    return dict(API_BASE_URL=app.config['API_BASE_URL'])
+ 
 @app.before_request
 def load_user():
     g.user_id = session.get("user_id")
-
-
+ 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True, threaded=True, use_reloader=False)
+ 
