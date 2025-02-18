b290d4197999aed238456e351a38d4f6bea101d0
RedShadow <marcuzg@gmail.com>
2025-02-18 16:04:08 +0100

    yup2

--- src/app.py
+++ src/app.py
@@ -2,38 +2,44 @@ from flask import Flask, session, g, render_template
 from flask_cors import CORS  # Import CORS
 from dotenv import load_dotenv
 import os
+import requests
 from database.mongo_connection import collection
 from utils import venvupdate
 from blueprint_register import register_blueprints  # Correct import statement
- 
+
 venvupdate.update_venv_and_requirements()
- 
+
 load_dotenv()
- 
+
 app = Flask(__name__, template_folder="templates", static_folder="static")
- 
+
 app.secret_key = os.getenv("SECRET_KEY")
- 
- 
+
 app.config["PREFERRED_URL_SCHEME"] = "https"
- 
-if os.getenv('FLASK_ENV') == 'production':
+
+def is_url_reachable(url):
+    try:
+        response = requests.get(url)
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
+
+if os.getenv('FLASK_ENV') == 'production' and is_url_reachable(os.getenv("PROD_BASE_URL")):
     app.config['API_BASE_URL'] = os.getenv("PROD_BASE_URL")
 else:
     app.config['API_BASE_URL'] = os.getenv("LOCAL_BASE_URL")
- 
+
 register_blueprints(app, "routes", [os.path.dirname(__file__) + "/routes"])
- 
+
 CORS(app)
- 
+
 @app.context_processor
 def inject_api_base_url():
     return dict(API_BASE_URL=app.config['API_BASE_URL'])
- 
+
 @app.before_request
 def load_user():
     g.user_id = session.get("user_id")
- 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True, threaded=True, use_reloader=False)
- 
