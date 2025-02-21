cddc8431c40bfbaa613c1e095e414ffcd0794300
Arinsz <arin.sz@hotmail.com>
2025-02-21 08:57:35 +0100

    add remember me

--- src/routes/signin.py
+++ src/routes/signin.py
@@ -1,5 +1,5 @@
 # 📌 Sign-in Route
-from flask import Flask, render_template, request, jsonify, session, Blueprint
+from flask import Flask, render_template, request, jsonify, session, Blueprint, redirect
 from werkzeug.security import check_password_hash
 from database.mongo_connection import collection
 
@@ -10,6 +10,8 @@ signin_bp = Blueprint("signin_bp", __name__)
 @signin_bp.route("/signin", methods=["GET", "POST"])
 def signin():
     if request.method == "GET":
+        if request.cookies.get("remember_me") == "true":
+            return redirect("/dashboard")
         return render_template("signin.html")
 
     if request.content_type != "application/json":
@@ -21,6 +23,7 @@ def signin():
     data = request.get_json()
     email = data.get("email", "").strip().lower()
     password = data.get("password", "")
+    remember = data.get("remember", False)
 
     users = list(collection.find({"email": email}))
 
@@ -29,6 +32,17 @@ def signin():
 
     session["user_id"] = str(users[0]["_id"])
     session["email"] = users[0]["email"]
-    session.permanent = True
+    session.permanent = remember
 
-    return jsonify({"message": "Login successful", "redirect_url": "podprofile"}), 200
+    response = jsonify(
+        {
+            "message": "Login successful",
+            "redirect_url": "podprofile" if not remember else "dashboard",
+        }
+    )
+    if remember:
+        response.set_cookie("remember_me", "true", max_age=30 * 24 * 60 * 60)  # 30 days
+    else:
+        response.delete_cookie("remember_me")
+
+    return response, 200
