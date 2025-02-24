bc24a757a3e90d0c614de3f431ee1807db5ad304
Arinsz <arin.sz@hotmail.com>
2025-02-24 10:39:46 +0100

    test

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ from routes.podtask import podtask_bp
 from routes.team import team_bp
 from routes.guest import guest_bp
 from routes.account import account_bp
-from routes.episodes import episodes_bp  # Use relative import
+from routes.episode import episode_bp  # Use relative import
 from dotenv import load_dotenv
 import os
 import logging
@@ -59,7 +59,7 @@ app.register_blueprint(podtask_bp)
 app.register_blueprint(team_bp)
 app.register_blueprint(guest_bp)
 app.register_blueprint(account_bp)
-app.register_blueprint(episodes_bp)
+app.register_blueprint(episode_bp)
 
 APP_ENV = os.getenv("APP_ENV", "production")  # Default to production
 
