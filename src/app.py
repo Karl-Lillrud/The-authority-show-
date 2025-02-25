5e379e89bd4a3e03a1f3d3a7f481b3a6ced19516
daniel svan <daniel_svan1@hotmail.com>
2025-02-25 19:52:58 +0100

    frontend folder

--- src/app.py
+++ src/app.py
@@ -26,16 +26,19 @@ from utils import venvupdate
 from database.mongo_connection import collection as team_collection
 from utils.email_utils import send_email
 
-# Checking if the environment variable is set to skip the virtual environment update
 if os.getenv("SKIP_VENV_UPDATE", "false").lower() not in ("true", "1", "yes"):
     venvupdate.update_venv_and_requirements()
 
 load_dotenv()
 
-app = Flask(__name__, template_folder="templates", static_folder="static")
+template_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), "Frontend", "templates")
+static_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), "Frontend", "static")
+
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+
 CORS(
     app,
-    resources={
+    resources={ 
         r"/*": {
             "origins": [
                 "http://192.168.0.4:8000",
@@ -43,12 +46,13 @@ CORS(
             ]
         }
     },
-)  # Enable CORS for specific origins
-
+)
 
 # These can cause  GET https://app.podmanager.ai/ 503 (Service Unavailable) error in the browser if not set
 app.secret_key = os.getenv("SECRET_KEY")
 app.config["PREFERRED URL SCHEME"] = "https"
+
+# Register blueprints for different routes
 app.register_blueprint(register_bp)
 app.register_blueprint(forgotpass_bp)
 app.register_blueprint(signin_bp)
@@ -63,24 +67,25 @@ app.register_blueprint(userstoteams_bp)
 app.register_blueprint(invitation_bp)
 app.register_blueprint(google_calendar_bp)
 
-APP_ENV = os.getenv("APP_ENV", "production")  # Default to production
+# Set the application environment (defaults to production)
+APP_ENV = os.getenv("APP_ENV", "production")
 
+# Set the API base URL depending on the environment
 API_BASE_URL = (
     "http://127.0.0.1:8000" if APP_ENV == "local" else "https://app.podmanager.ai/"
 )
-
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
+# Log the request with user info
 @app.before_request
 def load_user():
     g.user_id = session.get("user_id")
     logger.info(f"Request to {request.path} by user {g.user_id}")
 
-
+# Run the app
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0", port=8000, debug=False
