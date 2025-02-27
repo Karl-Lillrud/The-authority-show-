db88d920e386726a811f28d2cd6bdd81fcde7d75
Arinsz <arin.sz@hotmail.com>
2025-02-27 09:53:51 +0100

    add new files

---
+++ src/backend/routes/episodes.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, jsonify, request, g
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+episodes_bp = Blueprint("episodes_bp", __name__)
+
+# MongoDB Configuration
+MONGODB_URI = os.getenv("MONGODB_URI")
+DATABASE_NAME = "Podmanager"
+
+client = MongoClient(MONGODB_URI)
+database = client[DATABASE_NAME]
+
+
+@episodes_bp.route("/episodes/count_by_guest/<guest_id>", methods=["GET"])
+def count_episodes_by_guest(guest_id):
+    try:
+        count = database.episodes.count_documents({"guestId": guest_id})
+        return jsonify({"count": count}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@episodes_bp.route("/episodes/get_episodes_by_guest/<guest_id>", methods=["GET"])
+def get_episodes_by_guest(guest_id):
+    try:
+        episodes = list(database.episodes.find({"guestId": guest_id}))
+        for episode in episodes:
+            episode["_id"] = str(episode["_id"])
+        return jsonify({"episodes": episodes}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
