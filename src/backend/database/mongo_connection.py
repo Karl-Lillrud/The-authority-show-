1e1efde5818e7bb9a469becd123644aacb113ed9
Arinsz <arin.sz@hotmail.com>
2025-03-06 19:22:43 +0100

    Update mongo_connection.py

--- src/backend/database/mongo_connection.py
+++ src/backend/database/mongo_connection.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 
 # Initialize MongoDB Client
 try:
-    client = MongoClient(MONGODB_URI, ssl=True, ssl_cert_reqs="CERT_NONE")
+    client = MongoClient(MONGODB_URI)
     database = client[DATABASE_NAME]
     collection = database[COLLECTION_NAME]
     logger.info("MongoDB connection established successfully.")
