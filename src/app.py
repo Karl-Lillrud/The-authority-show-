cb78f69008fb71fba98c095b879c009ed4fc0dbf
PeterMolen <144238673+PeterMolen@users.noreply.github.com>
2025-03-10 15:30:27 +0100

    - merged my old requirments.txt with the new cloned version requiremnts.txt - extracted the miniconda yml file from my old branch to the new cloned project with better structure - setted up a new miniconda enviroment in the new cloned version - added transcription.py - added streamlit_trancription.py - updated .env.example for HF, AZURE, OPENAI & ELEVENLABS key, And how to run localy -updated database connection - added a /get_file/<file_id> route, to send and get audio files from mongodb -addad a a /get_video/<video_id> route, to send and video files from mongodb -changed all uplodad routes to be compatible with mongo db database.(took a very long time) i got at least 10 big errors for each route. 1, all routes had to be changed in both backend then also in frontend. fixed 2, alot of errors after routes have been renewed with old code, so old code also needed to be renewed. fixed 3, error of duplicates of files in database, mostly because of streamlit, fixed route list: /transcribe, /audio/enhancement, /audio_analysis, /clip_audio, /get_audio_info, /ai_cut_audio, /ai_videoedit,  /ai_videoanalysis,  /clip_video" -added so each file that gets uploaded to mongo db will be auto deleted after 24H -fixes: -fixed so trancription route now workds with videos aswell.

--- src/app.py
+++ src/app.py
@@ -17,6 +17,7 @@ from backend.routes.episode import episode_bp
 from backend.routes.podprofile import podprofile_bp  # Import the podprofile blueprint
 from backend.routes.frontend import frontend_bp  # Import the frontend blueprint
 from backend.routes.guest_to_eposide import guesttoepisode_bp
+from backend.routes.transcription import transcription_bp
 from dotenv import load_dotenv
 import os
 import logging
@@ -74,6 +75,7 @@ app.register_blueprint(episode_bp)
 app.register_blueprint(podprofile_bp)  # Register the podprofile blueprint
 app.register_blueprint(frontend_bp)  # Register the frontend blueprint
 app.register_blueprint(guesttoepisode_bp)
+app.register_blueprint(transcription_bp)
 # Set the application environment (defaults to production)
 APP_ENV = os.getenv("APP_ENV", "production")
 
