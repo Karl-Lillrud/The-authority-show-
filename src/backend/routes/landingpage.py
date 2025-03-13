4d98c4b9826b6ca8cb21c7639d0f322fcd72a0af
Robin Engvall <125352258+robinengvall@users.noreply.github.com>
2025-03-13 13:32:14 +0100

    host info, name fix

--- src/backend/routes/landingpage.py
+++ src/backend/routes/landingpage.py
@@ -21,6 +21,8 @@ def landingpage():
     # Get podcast title and description from the document.
     podcast_title = podcast_doc.get("podName", "Default Podcast Title") if podcast_doc else "Default Podcast Title"
     podcast_description = podcast_doc.get("description", "Default Podcast Description") if podcast_doc else "Default Podcast Description"
+    host_name = podcast_doc.get("hostName", "Error with name") if podcast_doc else "Default Podcast Description"
+
     
     # You can also still query episodes if needed:
     episodes_collection = db["Episodes"]
@@ -30,7 +32,8 @@ def landingpage():
         'landingpage/landingpage.html',
         podcast_title=podcast_title,
         podcast_description=podcast_description,
-        episodes=episodes
+        episodes=episodes,
+        host_name=host_name
     )
 @landingpage_bp.route('/episode', methods=['GET'])
 def episode():
