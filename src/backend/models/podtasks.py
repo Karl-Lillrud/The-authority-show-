4791e7af2fec9f615f6d58ea76b3256c2b3e26b5
daniel svan <daniel_svan1@hotmail.com>
2025-03-23 14:06:48 +0100

    closing old branch

--- src/backend/models/podtasks.py
+++ src/backend/models/podtasks.py
@@ -3,14 +3,15 @@ from marshmallow import Schema, fields
 class PodtaskSchema(Schema):
     id = fields.Str()
 
-    # Optional podcast, episode, team, and members
-    podcastId = fields.Str(allow_none=True)    # Optional for general tasks
+    # Optional podcast, episode, team, and members, guestId. "Team can make a task for a team and"
+    #members can be assigned to a task towards episode or podcast, guest.
+    podcastId = fields.Str(allow_none=True)    # Optional 
     episodeId = fields.Str(allow_none=True)    # Optional
     teamId = fields.Str(allow_none=True)       # Optional
     members = fields.List(fields.Str(), allow_none=True)  # Optional list of user IDs
-
+    guestId = fields.Str(allow_none=True)      # Optional
     name = fields.Str(required=True)
-    action = fields.List(fields.Str())
+    action = fields.List(fields.Str()) #should manly be manual until code or system for automation is in place
     dayCount = fields.Int()
     description = fields.Str()
     actionUrl = fields.Url(allow_none=True)
