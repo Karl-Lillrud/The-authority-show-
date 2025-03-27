1d82e75587338338970d6b311bc5529b9ed47862
jduangpradap <duangpradapjeeno@gmail.com>
2025-03-27 16:13:56 +0100

    problem with how matplot is used
    fixed the problem with how matplot is used

--- src/backend/routes/transcription.py
+++ src/backend/routes/transcription.py
@@ -6,6 +6,8 @@ import subprocess
 from datetime import datetime
 from io import BytesIO
 from flask import Blueprint, request, jsonify
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import soundfile as sf
