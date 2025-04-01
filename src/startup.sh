97997b3ee929002201d23ce83521683e0f23bbad
Arinsz <arin.sz@hotmail.com>
2025-04-01 11:27:08 +0200

    test

--- src/startup.sh
+++ src/startup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-gunicorn --bind 0.0.0.0:80 app:app &  # Changed to port 80
-streamlit run backend/routes/transcript/streamlit_transcription.py --server.port 8501 &
+
+# Start Gunicorn (Flask) på port 8000
+gunicorn --chdir src --bind=0.0.0.0:8000 --timeout 600 app:app &
+
+# Start Streamlit på port 8501
+streamlit run src/backend/routes/transcript/streamlit_transcription.py --server.port 8501 &
+
+# Håll scriptet igång
 wait
