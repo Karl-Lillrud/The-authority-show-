4d665c928ac07a8e34eea479a48280403855162f
Arinsz <arin.sz@hotmail.com>
2025-04-01 19:26:48 +0200

    test

--- .github/workflows/startup.sh
+++ .github/workflows/startup.sh
@@ -2,12 +2,6 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-# Check available disk space
-if [ "$(df / | tail -1 | awk '{print $4}')" -lt 1048576 ]; then
-  echo "Error: Not enough disk space available."
-  exit 1
-fi
-
 # Start gunicorn in the background
 gunicorn --chdir src --bind=0.0.0.0 --timeout 600 app:app &
 
