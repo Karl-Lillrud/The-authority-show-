b38dc26620d077a27abf1498480f435214ee0c1f
Arinsz <arin.sz@hotmail.com>
2025-04-01 23:18:28 +0200

    testar

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Clean up unnecessary files to free up space
+rm -f /home/site/wwwroot/output.tar.gz
+
+# Start gunicorn in the background
+gunicorn --chdir src --bind=0.0.0.0 --timeout 600 app:app &
+
+# Run streamlit
+streamlit run src/backend/routes/transcript/streamlit_transcription.py
