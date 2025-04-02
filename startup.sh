5cfef386476afbdc9e1f3f2c44b289ce2bace611
Arinsz <arin.sz@hotmail.com>
2025-04-02 11:18:22 +0200

    test

--- startup.sh
+++ startup.sh
@@ -2,11 +2,8 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-# Check if the 'src' directory exists
-if [ ! -d "/home/site/wwwroot/src" ]; then
-  echo "❌ Error: '/home/site/wwwroot/src' directory not found. Ensure the application is deployed correctly."
-  exit 1
-fi
+# Start gunicorn in the background
+gunicorn --chdir src --bind=0.0.0.0 --timeout 600 app:app &
 
 # Run streamlit
-streamlit run /home/site/wwwroot/src/backend/routes/transcript/streamlit_transcription.py
+streamlit run src/backend/routes/transcript/streamlit_transcription.py
