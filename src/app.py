a6dca9bd2bd66eeadb5c31bde0027e8f8ae4677a
Arinsz <arin.sz@hotmail.com>
2025-04-03 12:02:25 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -162,5 +162,5 @@ if __name__ == "__main__":
 
     port = int(
         os.environ.get("PORT", 5000)
-    )  # Använd port givet av Azure om tillgänglig
+    )  # Använd port givet av Azure om tiSSllgänglig
     app.run(host="0.0.0.0", port=port)
