b39278da05675ac3bdcc2e29f8ba2db61fad1833
Arinsz <arin.sz@hotmail.com>
2025-04-19 14:00:00 +0200

    fix

--- src/app.py
+++ src/app.py
@@ -31,10 +31,11 @@ from backend.utils import venvupdate
 from backend.database.mongo_connection import collection
 from backend.routes.Mailing_list import Mailing_list_bp
 from backend.routes.user import user_bp
-#from backend.routes.highlight import highlights_bp
-#from backend.routes.audio_routes import audio_bp
-#from backend.routes.video_routes import video_bp
-#from backend.routes.transcription import transcription_bp
+
+# from backend.routes.highlight import highlights_bp
+# from backend.routes.audio_routes import audio_bp
+# from backend.routes.video_routes import video_bp
+# from backend.routes.transcription import transcription_bp
 from colorama import Fore, Style, init  # Import colorama for styled logs
 
 if os.getenv("SKIP_VENV_UPDATE", "false").lower() not in ("true", "1", "yes"):
@@ -88,9 +89,9 @@ app.register_blueprint(episode_bp)
 app.register_blueprint(podprofile_bp)  # Register the podprofile blueprint
 app.register_blueprint(frontend_bp)  # Register the frontend blueprint
 app.register_blueprint(guesttoepisode_bp)
-#app.register_blueprint(transcription_bp, url_prefix="/transcription")
-#app.register_blueprint(audio_bp)
-#app.register_blueprint(video_bp)
+# app.register_blueprint(transcription_bp, url_prefix="/transcription")
+# app.register_blueprint(audio_bp)
+# app.register_blueprint(video_bp)
 app.register_blueprint(billing_bp)
 app.register_blueprint(
     guest_form_bp, url_prefix="/guest-form"
@@ -121,7 +122,7 @@ logger.info(f"{Fore.GREEN}========================================")
 logger.info(f"{Fore.CYAN}📧 Email Configuration:")
 logger.info(f"{Fore.BLUE}EMAIL_USER: {os.getenv('EMAIL_USER', 'Not Set')}")
 logger.info(
-    f"{Fore.BLUE}EMAIL_PASS: {'**** **** **** ****' if os.getenv('EMAIL_PASS') else 'Not Set'}"
+    f"{Fore.BLUE}EMAIL_PASSWORD: {'**** **** **** ****' if os.getenv('EMAIL_PASSWORD') else 'Not Set'}"
 )
 logger.info(f"{Fore.GREEN}========================================")
 logger.info(f"{Fore.CYAN}🚀 Server is running!")
