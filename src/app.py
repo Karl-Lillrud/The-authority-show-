1c51647e78489224837ab55543c727b4f5a8ab89
Robin Engvall <125352258+robinengvall@users.noreply.github.com>
2025-04-24 10:26:54 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -90,9 +90,9 @@ app.register_blueprint(episode_bp)
 app.register_blueprint(podprofile_bp)  # Register the podprofile blueprint
 app.register_blueprint(frontend_bp)  # Register the frontend blueprint
 app.register_blueprint(guesttoepisode_bp)
- app.register_blueprint(transcription_bp, url_prefix="/transcription")
- app.register_blueprint(audio_bp)
- app.register_blueprint(video_bp)
+app.register_blueprint(transcription_bp, url_prefix="/transcription")
+app.register_blueprint(audio_bp)
+app.register_blueprint(video_bp)
 app.register_blueprint(billing_bp)
 app.register_blueprint(
     guest_form_bp, url_prefix="/guest-form"
