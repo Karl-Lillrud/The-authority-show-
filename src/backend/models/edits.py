ac9f75a0be6cc1b3b1fd84bc2c41804713cce241
Arinsz <arin.sz@hotmail.com>
2025-04-29 14:50:47 +0200

    fix

--- src/backend/models/edits.py
+++ src/backend/models/edits.py
@@ -1,12 +1,13 @@
 from marshmallow import Schema, fields
 
-class ClipsSchema(Schema):
+
+class EditsSchema(Schema):
     id = fields.Str()
     episodeId = fields.Str(required=True)
-    clipName = fields.Str(required=True)
+    editName = fields.Str(required=True)
     duration = fields.Int()
     createdAt = fields.DateTime()
     editedBy = fields.List(fields.Str())
-    clipUrl = fields.Url()
+    editUrl = fields.Url()
     status = fields.Str()
     tags = fields.List(fields.Str())
