c2bc8bf10891dd02c4f55dbba109925ba8429a97
RedShadow[Sweden] <168818119+RedShadowSwe@users.noreply.github.com>
2025-04-30 13:28:53 +0200

    updated teams.py

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ FROM python:3.9-slim
 # Set the working directory in the container
 WORKDIR /app
 
+# Copy the requirements.txt into the container at /app
+COPY ./src/requirements.txt /app/
+
 # Install dependencies
-COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy the current directory contents into the container at /app
--- deploy.sh
+++ deploy.sh
@@ -101,6 +101,10 @@ az webapp config container set --name $WEBAPP_NAME --resource-group $RESOURCE_GR
   --container-registry-user "$ACR_USERNAME" \
   --container-registry-password "$ACR_PASSWORD"
 
-# Step 11: Check the status of the Web App
+# Step 11: Restart Web App to trigger image pull from ACR
+echo "Restarting Web App '$WEBAPP_NAME' to trigger image pull from ACR..."
+az webapp restart --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP
+
+# Step 12: Check the status of the Web App
 echo "Web App '$WEBAPP_NAME' is deployed successfully. Checking the status..."
 az webapp show --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP --output table
