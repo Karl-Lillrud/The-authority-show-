75f8ff06125041e85e0033e6e1d96036fc0887a3
Pontus Flodström <pontaflod95@hotmail.com>
2025-05-07 13:37:01 +0200

    Added onScroll event to change which nav item is display as active depending on what section the user is on. + Small design fixes

--- src/Frontend/static/js/index/index.js
+++ src/Frontend/static/js/index/index.js
@@ -1,4 +1,7 @@
-/* Hamburger menu */
+/* * * * * * * * */
+/*  Navigation   */
+/* * * * * * * * */
+
 function toggleHamburger(x) {
   x.classList.toggle("change"); // Switch the hamburger icon to an "X" icon
 
@@ -9,3 +12,23 @@ function toggleHamburger(x) {
     item.classList.toggle("change"); // Toggle the visibility of the navigation items
   });
 }
+
+window.onscroll = () => { // Change the active class of the navigation items based on scroll position
+  const sections = document.querySelectorAll("section");
+  const navLi = document.querySelectorAll("nav ul.nav-links li");
+  var current = "";
+  var midOfDevice = window.innerHeight / 2;
+
+  sections.forEach((section) => { // Loop through each section to determine which one is currently in view
+    const sectionTop = section.offsetTop - midOfDevice;
+    if (scrollY >= sectionTop ) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => { // Loop through each navigation item to update its active state
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
