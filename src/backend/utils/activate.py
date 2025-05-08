a906a5a17ff1e695371df1bebf3c23a5fe902ceb
RedShadow[Sweden] <168818119+RedShadowSwe@users.noreply.github.com>
2025-05-08 23:19:10 +0200

    .

--- src/backend/utils/activate.py
+++ src/backend/utils/activate.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 # Constants
 API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000").rstrip('/')
-XML_FILE_PATH = os.getenv("ACTIVATION_XML_FILE_PATH", "../test.xml")
+XML_FILE_PATH = os.getenv("ACTIVATION_XML_FILE_PATH", "../../frontend/static/scraped.xml")
 
 def load_podcasts_from_xml(file_path):
     """Load podcasts from an XML file."""
