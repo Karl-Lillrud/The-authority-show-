f650a71578ab18d330a3330a7316d9cb1bde404c
Arinsz <arin.sz@hotmail.com>
2025-05-21 14:14:55 +0200

    Update __init__.py

--- src/backend/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_socketio import SocketIO
-
-# Initialize SocketIO without the app object initially.
-# It will be initialized with the app in app.py using socketio.init_app(app).
-# cors_allowed_origins can be set here or during init_app.
-socketio = SocketIO(cors_allowed_origins="*")
-
