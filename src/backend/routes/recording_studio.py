78f4efab8d69db5b84521aa3111983c5c53b8244
alexizsz <alexissalazar9@gmail.com>
2025-05-21 11:56:30 +0200

    Update recording_studio.py

--- src/backend/routes/recording_studio.py
+++ src/backend/routes/recording_studio.py
@@ -1,14 +1,13 @@
-from flask import request, jsonify, Blueprint, url_for, render_template
-from backend.database.mongo_connection import database
-from backend.utils.email_utils import send_email
-from backend.services.invitation_service import InvitationService
-from datetime import datetime
 from flask import Blueprint, request
 from flask_socketio import join_room, leave_room, emit
 from flask_socketio import SocketIO
 from .. import socketio
 from backend.services.recording_studio_service import recording_studio_service
 
+from flask import request, jsonify, Blueprint, url_for, render_template
+from backend.database.mongo_connection import database
+from backend.services.invitation_service import InvitationService
+from datetime import datetime
 
 recording_studio_bp = Blueprint("recording_studio_bp", __name__)
 
@@ -16,6 +15,41 @@ recording_studio_bp = Blueprint("recording_studio_bp", __name__)
 invitations_collection = database.Invitations
 episodes_collection = database.Episodes
 guests_collection = database.Guests
+
+# Socket.IO event: handle user connection
+@socketio.on('connect')
+def handle_connect():
+    emit('connected', {'message': 'Connected to server'})
+
+# Socket.IO event: join a room
+@socketio.on('join_room')
+def handle_join_room(data):
+    room = data.get('room')
+    user = data.get('user')
+    join_room(room)
+    users = recording_studio_service.join_room(room, user)
+    emit('room_joined', {'room': room, 'user': user, 'users': users}, room=room)
+
+# Socket.IO event: leave a room
+@socketio.on('leave_room')
+def handle_leave_room(data):
+    room = data.get('room')
+    user = data.get('user')
+    leave_room(room)
+    users = recording_studio_service.leave_room(room, user)
+    emit('room_left', {'room': room, 'user': user, 'users': users}, room=room)
+
+# Socket.IO event: get users in a room
+@socketio.on('get_users_in_room')
+def handle_get_users_in_room(data):
+    room = data.get('room')
+    users = recording_studio_service.get_users_in_room(room)
+    emit('users_in_room', {'room': room, 'users': users})
+
+# Optionally, handle disconnect
+def handle_disconnect():
+    emit('disconnected', {'message': 'Disconnected from server'})
+socketio.on_event('disconnect', handle_disconnect)
 
 # POST /invite - Generate and send invitation email for a session
 @recording_studio_bp.route('/invite', methods=['POST'])
@@ -128,45 +162,3 @@ def recording_studio():
     """
     return render_template('recordingstudio/recording_studio.html')
 
-from flask import Blueprint, request
-from flask_socketio import join_room, leave_room, emit
-from flask_socketio import SocketIO
-from .. import socketio
-from backend.services.recording_studio_service import recording_studio_service
-
-recording_studio_bp = Blueprint('recording_studio', __name__)
-
-# Socket.IO event: handle user connection
-@socketio.on('connect')
-def handle_connect():
-    emit('connected', {'message': 'Connected to server'})
-
-# Socket.IO event: join a room
-@socketio.on('join_room')
-def handle_join_room(data):
-    room = data.get('room')
-    user = data.get('user')
-    join_room(room)
-    users = recording_studio_service.join_room(room, user)
-    emit('room_joined', {'room': room, 'user': user, 'users': users}, room=room)
-
-# Socket.IO event: leave a room
-@socketio.on('leave_room')
-def handle_leave_room(data):
-    room = data.get('room')
-    user = data.get('user')
-    leave_room(room)
-    users = recording_studio_service.leave_room(room, user)
-    emit('room_left', {'room': room, 'user': user, 'users': users}, room=room)
-
-# Socket.IO event: get users in a room
-@socketio.on('get_users_in_room')
-def handle_get_users_in_room(data):
-    room = data.get('room')
-    users = recording_studio_service.get_users_in_room(room)
-    emit('users_in_room', {'room': room, 'users': users})
-
-# Optionally, handle disconnect
-def handle_disconnect():
-    emit('disconnected', {'message': 'Disconnected from server'})
-socketio.on_event('disconnect', handle_disconnect)
